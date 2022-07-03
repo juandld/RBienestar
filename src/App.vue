13fe79afdfb0f19cdf6eecad9c5e89f9890afe05
Juan David López <35150736+juandld@users.noreply.github.com>
2022-07-02 21:26:23 -0500

    Initial commit, Router installed/Nav Initiated

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  },
+}
+</script>
+
+<template>
+  <NavBar/>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <header :class="{'scrolled-nav': scrollPosition}">
+    <nav>
+      <div>
+        <img src="src\assets\logo.svg" alt="RBienestar Logo">
+      </div>  
+      <ul v-show="!mobile" class="navigation">
+        <li><router-link :to="{name: 'home'}">Inicio</router-link></li>
+        <li><router-link :to="{name: 'about'}">Sobre nosotros</router-link></li>
+      </ul>
+      <div class="icon">
+        <i @click="toggleMobileNav" v-show="mobile" class="fa fa-bars" :class="{'icon-active' : mobileNav}"></i>
+        <transition name="mobile-nav">
+          <ul v-show="mobileNav" class="dropdown-nav">
+            <li><router-link :to="{name: 'home'}">Inicio</router-link></li>
+            <li><router-link :to="{name: 'about'}">Sobre nosotros</router-link></li>
+          </ul>
+        </transition>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+header {
+  background: rgba(252, 252, 252, 0.3);
+  backdrop-filter: blur(14px);
+  /* Note: backdrop-filter has minimal browser support */
+  top: 0;
+  border-radius: 0px 0px 20px 20px;
+  position: fixed;
+  z-index: 99;
+  width: 100%;
+  transition: .5s ease all;
+
+}
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    }
+</style>

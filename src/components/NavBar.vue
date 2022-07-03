c27bc3b8e18db8f10c0fd1c6312e411691b7218b
Juan David López <35150736+juandld@users.noreply.github.com>
2022-07-03 13:56:19 -0500

    Switching workstation advancement

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,15 +5,15 @@
         <img src="src\assets\logo.svg" alt="RBienestar Logo">
       </div>  
       <ul v-show="!mobile" class="navigation">
-        <li><router-link :to="{name: 'home'}">Inicio</router-link></li>
-        <li><router-link :to="{name: 'about'}">Sobre nosotros</router-link></li>
+        <li class="link"><router-link :to="{name: 'home'}">Inicio</router-link></li>
+        <li class="link"><router-link :to="{name: 'about'}">Sobre nosotros</router-link></li>
       </ul>
       <div class="icon">
         <i @click="toggleMobileNav" v-show="mobile" class="fa fa-bars" :class="{'icon-active' : mobileNav}"></i>
         <transition name="mobile-nav">
           <ul v-show="mobileNav" class="dropdown-nav">
-            <li><router-link :to="{name: 'home'}">Inicio</router-link></li>
-            <li><router-link :to="{name: 'about'}">Sobre nosotros</router-link></li>
+            <li class="link"><router-link  :to="{name: 'home'}">Inicio</router-link></li>
+            <li class="link"><router-link  :to="{name: 'about'}">Sobre nosotros</router-link></li>
           </ul>
         </transition>
       </div>
@@ -24,6 +24,13 @@
 <script>
 export default {
   name: "NavBar",
+  data() {
+    return {
+      scrollPosition: null,
+      mobile: false,
+      mobileNav: null,
+    }
+  },
 };
 </script>
 
@@ -45,5 +52,29 @@ header {
     justify-content: space-between;
     align-items: center;
     flex-direction: row;
+    padding: 12px;
+    transition: .5s ease all;
+    width: 90%;
     }
+
+   @media(min-width: 1140px) {
+      nav {
+        max-width: 1140px;
+      }
+    }
+  .link {
+    text-decoration: none;
+    color: #000;
+    list-style-type: none;
+  }
+
+  .navigation {
+    display: flex;
+    
+    flex-direction: row;
+    padding: 12px;
+    transition: .5s ease all;
+    width: 90%;
+  }
+
 </style>

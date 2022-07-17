a399a59b33b3d5019b4dd8880bc5a50c658f140e
Juan David López <35150736+juandld@users.noreply.github.com>
2022-07-17 11:03:40 -0500

    Carousel implemented

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,22 +5,39 @@
         <h2>En Toda Colombia</h2>
         <h1>NIKKEN</h1>
     </div>
-    
 </div>
-
+  <div class="home">
+    <Carousel
+      :navigation="true"
+      :pagination="true"
+      :startAutoPlay="false"
+      :timeout="5000"
+      class="carousel"
+      v-slot="{ currentSlide }"
+    >
+      <Slide v-for="(slide, index) in carouselSlides" :key="index">
+        <div v-show="currentSlide === index + 1" class="slide-info">
+          <img :src="`src/assets/placeholders/${slide}.jpg`" alt="" />
+        </div>
+      </Slide>
+    </Carousel>
+  </div>
 </template>
 
 <script>
-
+import Carousel from "../components/imageView/Carousel.vue";
+import Slide from "../components/imageView/Slide.vue";
 export default {
-    name: 'Home',
-    components: {
-
-    }
-}
+  name: "Home",
+  components: { Carousel, Slide },
+  setup() {
+    const carouselSlides = ["item1", "item2", "item3"];
+    return { carouselSlides };
+  },
+};
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
  .HeadlineContainer {
     justify-content: center;
     align-items: center;
@@ -35,5 +52,22 @@ export default {
   transform: translate(-50%, -50%);
 }
 
-
+.carousel {
+  position: relative;
+  max-height: 100vh;
+  height: 100vh;
+  .slide-info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-height: 100%;
+    height: 100%;
+    img {
+      min-width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
 </style>
